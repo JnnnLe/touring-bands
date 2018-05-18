fbfbf8017b54379e48b0bd9a3a9446eb8bd8e64e
Jennifer Le <jnnnle.sf@gmail.com>
2018-05-17 22:57:03 -0700

    Set State to input value

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Header from './components/Header/Header';
 import Search from './components/Search/Search';
+import config from './config';
 
 import './App.css';
 
+//get Artist
+
 class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      queryArtist: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.getArtist = this.getArtist.bind(this);
+  }
+
+  handleChange = (event) => {
+    this.setState({ queryArtist : event.target.value})
+    console.log('State: ', this.state)
+  }
+  // fn for cleaning up query for bands with long names
+
+  getArtist = () => {
+    axios.get(`https://rest.bandsintown.com/artists/${this.state.queryArtist}/events?app_id=${config.apiKey}`)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  // formatTourDates = () => {}
+
   render() {
+    console.log(this.getArtist())
     return (
       <div className="App">
         <Header />
-        <Search />
+        <Search 
+          getArtist={this.getArtist}
+          handleChange={this.handleChange}
+        />
       </div>
     );
   }
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,39 +1,15 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-import config from '../../config';
-
-class Search extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      queryArtist : 'pink'
-    }
-    this.getArtist = this.getArtist.bind(this);
-  }
-
-  getArtist = () => {
-    axios.get(`https://rest.bandsintown.com/artists/${this.state.queryArtist}/events?app_id=${config.apiKey}`)
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-
-  render() {
-    this.getArtist();
-    return (
-      <div>
-        <form>
-          <input type="text" />
-          <br />
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    )
-  }
+const Search = (props) => {
+  return (
+    <div>
+      <form>
+        <input type="text" onChange={props.handleChange} />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  )
 }
 
 export default Search;
